aaf6e5f951a7c268697f448b2266c2886ecff18e
JohnMugambi <50951858+JohnMugambi@users.noreply.github.com>
2020-07-18 12:23:45 +0300

    Edits

--- scripts/index script.js
+++ scripts/index script.js
@@ -1,4 +1,32 @@
 /* Fade in out of header and main*/
+
+var scrollEffect = document.getElementById('scroll-fade');
+window.addEventListener('scroll', function(){
+    var scroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    scrollEffect.style.opacity = Math.max(0, Math.min(1,-scroll/300 +1.5 ));
+    
+});
+
+window.onscroll = function(){
+    var top = window.scrollY;
+    
+    if (top > 520){
+        this.scrollingInto.style.opacity = this.Math.min(1, Math.min(1, top/100 - 3.5));               
+    }else{               
+        this.scrollingInto.style.opacity = this.Math.min(1, Math.min(1, top/120 - 2));
+    }
+}
+
+var scrollToTop = document.querySelector('.goTop');
+scrollToTop.addEventListener('click', function(){
+    smoothScroll('.nav-bar', 1000);
+});
+
+var viewSkills = document.querySelector(".view-skills");
+viewSkills.addEventListener('click',function(){
+    smoothScroll('.skills-section', 1000);
+});
+
 function smoothScroll( target, duration){
     var target = document.querySelector(target);
     var targetPosition = target.getBoundingClientRect().top;
@@ -28,29 +56,3 @@ function smoothScroll( target, duration){
 
     requestAnimationFrame(animation);
 }
-var scrollEffect = document.getElementById('scroll-fade');
-window.addEventListener('scroll', function(){
-    var scroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    scrollEffect.style.opacity = Math.max(0, Math.min(1,-scroll/300 +1.5 ));
-    
-});
-
-window.onscroll = function(){
-    var top = window.scrollY;
-    
-    if (top > 520){
-        this.scrollingInto.style.opacity = this.Math.min(1, Math.min(1, top/100 - 3.5));               
-    }else{               
-        this.scrollingInto.style.opacity = this.Math.min(1, Math.min(1, top/120 - 2));
-    }
-}
-
-var scrollToTop = document.querySelector('.goTop');
-scrollToTop.addEventListener('click', function(){
-    smoothScroll('.nav-bar', 1000);
-});
-
-var viewSkills = document.querySelector(".view-skills");
-viewSkills.addEventListener('click',function(){
-    smoothScroll('.skills-section', 1000);
-})
